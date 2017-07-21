07bb3635132b2609757e69d4c09cfd8b89ae31e8
slagus <amanbheley.pcte@gmail.com>
2017-07-21 19:42:53 +0530

    Changes for angular backend data bindings

--- index.php
+++ index.php
@@ -9,16 +9,80 @@
 		<script src="js/app.js"></script>
 		<title>Trello Board</title>
 		
+		<style>
+			.textbox{
+				padding:8px 10px;
+				border:1px solid #c1c1c1;
+				border-radius:2px;
+			}
+			
+			h1,h4{color:#fff;}
+			
+			.button{
+				padding:8.5px 10px;
+				border:0px;
+				border-radius:2px;
+				background:#fc2;
+			}
+			.select{
+				padding:7px 10px;
+				border:1px solid #c1c1c1;
+				border-radius:2px;
+			}
+		</style>
 
 	</head>
 	<body>
-		
 		<div ng-controller="ClbBoardController">
+			
+			<div style="padding:20px;">
+				
+				<h1>CARD LIST BOARD</h1>
+				<div style="margin-bottom:10px;">
+				
+					<h4>Add New List</h4>
+					<input type="text" class="textbox" placeholder="New List Title" ng-model="newlistTitle"/>
+					<button ng-click="createList()" class="button">Create List</button>
+					<h4>Add New card</h4>
+					<input type="text" class="textbox" placeholder="List Index" ng-model="newCard.targetlistIndex"/>
+					<input type="text" class="textbox" placeholder="Card Title" ng-model="newCard.title"/>
+					<select type="text" class="select" placeholder="Card Type" ng-model="newCard.type">
+						<option value="">Select Type</option>
+						<option value="{{key}}" ng-repeat="(key, type) in cardTypes">{{type.title}}</option>
+					</select>
+					<button ng-click="createCard()" class="button">Create Card</button>
+					
+					<h4>Edit List</h4>
+					<input type="text" class="textbox" placeholder="List Index" ng-change="getList()" ng-model="editListIndex"/>
+					<input type="text" class="textbox" placeholder="List Title" ng-model="editList.title"/>
+					<button ng-click="updateList()" class="button">Update List</button>
+					
+					<h4>Edit Card</h4>
+					<input type="text" class="textbox" placeholder="List Index" ng-model="editCardTargetlistIndex"/>
+					<input type="text" class="textbox" placeholder="Card Index" ng-change="getCard()" ng-model="editCardTargetcardIndex"/>
+					<input type="text" class="textbox" placeholder="Card Title" ng-model="editCard.title"/>
+					<input type="text" class="textbox" placeholder="Priority" ng-model="editCard.priority"/>
+					<select type="text" class="select" placeholder="Card Type" ng-model="editCard.type">
+						<option value="">Select Type</option>
+						<option value="{{key}}" {{editCard.type == key ? 'selected'}} ng-repeat="(key, type) in cardTypes">{{type.title}}</option>
+					</select>
+					<button ng-click="updateCard()" class="button">Update Card</button>
+					
+				
+				</div>
+				
+				
+				<button ng-click="deleteLists()" class="button">Delete Lists</button>
+				<button ng-click="lists = lists2" class="button">Overwite List</button>
+				<button ng-click="cardUpdateCallback()" class="button">Card Update Callback Test</button>
+			</div>
+			
 			<clb-board 
 				clb-lists="lists" 
 				clb-card-types="cardTypes"
 				on-update="cardUpdateCallback(item)"
-				priority-index="priority"
+				on-card-click="cardClicked(card)"
+				priority-index="'priority'"
 			></clb-board>
 		</div>
 		
--- js/app.js
+++ js/app.js
@@ -1,14 +1,14 @@
 angular.module('clbBoard', []).controller('ClbBoardController', function($scope){
 	$scope.lists = [
 		{ 
-			title : 'Worker1',
+			title : 'Worker 1',
 			cards : [
 				{title: "Create new database with some new fresh columns and try to mainatian the speed and performance as much improved as we can.", type : 'DONE', priority : 5},
 				{title: "Need to fix the dashboard issue", type : 'BUG', priority : 3}
 			]
 		},
 		{ 
-			title : 'Worker2',
+			title : 'Worker 2',
 			cards : [
 				{title: "Coputer needs to be repaired", type : 'BUG', priority : 1},
 				{title: "3rd layout", type : 'PENDING', priority : 3},
@@ -16,13 +16,50 @@ angular.module('clbBoard', []).controller('ClbBoardController', function($scope)
 			]
 		},
 		{
-			title : 'Worker3'
+			title : 'Worker 3',
+			cards : []
 			
 		}
 	];
 	
+	$scope.lists2 = [
+		{ 
+			title : 'In stock',
+			cards : [
+				{title: "Peripherals", type : 'DONE', priority : 5},
+				{title: "mousepads", type : 'DONE', priority : 3}
+			]
+		},
+		{ 
+			title : 'Out of stock',
+			cards : [
+				{title: "Laptops", type : 'BUG', priority : 1},
+				{title: "Data Cables", type : 'BUG', priority : 3},
+				{title: "LCD", type : 'BUG', priority : 2}
+			]
+		},
+		{
+			title : 'Potential Stock',
+			cards : []
+			
+		}
+	];
+	
+	$scope.cardClicked = function(item){
+		console.log(item);
+	};
+	
 	$scope.cardUpdateCallback = function(item){
-		$scope.lists[0].cards[item.prevIndex].priority = item.newIndex + 1;
+		if ($scope.lists[item.prevListIndex].cards) {
+			var injectableJson = $scope.lists[item.prevListIndex].cards.splice(item.prevIndex, 1);			
+			console.log(injectableJson);
+			$scope.lists[item.newListIndex].cards.splice(item.newIndex, 0, injectableJson[0]);			
+		}
+		
+		console.log('Item prevIndex', item.prevIndex + 1);
+		console.log('Item newIndex', item.newIndex + 1);
+		console.log('Item prevListIndex', item.prevListIndex + 1);
+		console.log('Item newListIndex', item.newListIndex + 1);
 	};
 	
 	$scope.cardTypes = {
@@ -31,6 +68,49 @@ angular.module('clbBoard', []).controller('ClbBoardController', function($scope)
 		PENDING : {title : 'Pending task', colorCode: '#f4a408'},
 	}
 	
+	$scope.deleteLists = function(){
+		$scope.lists = [];
+	};
+	
+	$scope.createList = function(){
+		$scope.lists.push({
+			title : $scope.newlistTitle,
+			cards : []
+		});
+	};
+	$scope.newCard = {targetlistIndex: 1, title: "", type : "", priority : 999};
+	$scope.createCard = function(){
+		$scope.lists[$scope.newCard.targetlistIndex - 1].cards.push({
+			title : $scope.newCard.title,
+			type : $scope.newCard.type,
+		});
+	};
+	
+	$scope.getList = function(){
+		var index = $scope.editListIndex - 1;
+		$scope.editList = $scope.lists[index];
+	};
+	$scope.editList = {};
+	$scope.updateList = function(){
+		var targetList 		= $scope.lists[$scope.editListIndex - 1];
+		targetList.title 	= $scope.editList.title;
+	};
+	
+	$scope.getCard = function(){
+		var listIndex = $scope.editCardTargetlistIndex - 1;
+		var cardIndex = $scope.editCardTargetcardIndex - 1;
+		$scope.editCard = $scope.lists[listIndex].cards[cardIndex];
+	};
+	$scope.editCard = {};
+	$scope.updateCard = function(){
+		var listIndex = $scope.editCardTargetlistIndex - 1;
+		var cardIndex = $scope.editCardTargetcardIndex - 1;
+		
+		var targetCard = $scope.lists[listIndex].cards[cardIndex];
+		targetCard.title = $scope.editCard.title;
+		targetCard.type = $scope.editCard.type;	
+	};
+	
 })
 .directive('clbBoard', function() {
 	return {
@@ -38,48 +118,63 @@ angular.module('clbBoard', []).controller('ClbBoardController', function($scope)
 		scope: {
 			clbLists: '=',
 			clbCardTypes: '=',
-			onUpdateCallback: '&onUpdate',
-			priorityIndex: '='
+			onUpdateCallback: '&?onUpdate',
+			priorityIndex: '=',
+			onCardClickCallback: '&?onCardClick'
 		},
-		template : '<div class="board">'+
-			'<div clb-list ng-repeat="list in clbLists" class="list"></div>'+	
+		template : '<div class="clb-board">'+
+			'<div clb-list data-list-index="{{key}}" ng-repeat="(key, list) in clbLists" class="clb-list"></div>'+	
 		'</div>'
 	};
 }).directive('clbList', function() {
   return {
     restrict: 'A',
 	link: function($scope, element, attrs) {
-		$( element ).find('.sortable').sortable({
-		  placeholder: "ui-state-highlight",
-		  connectWith: ".connectedSortable",
+		$( element ).find('.clb-sortable').sortable({
+		  placeholder: "clb-board-placeholder",
+		  connectWith: ".clb-connected-sortable",
 		  start: function(event, ui){
+			  var prevListIndex = $(ui.item).parents('[data-list-index]').data('list-index');
+			  ui.item.prevListIndex = prevListIndex;
 			  ui.item.prevIndex = ui.item.index();
 			  var height = $(ui.item).outerHeight();
-			  $(".ui-state-highlight").height(height);
-			  $(ui.item).addClass('dragging');
+			  $(".clb-board-placeholder").height(height);
+			  $(ui.item).addClass('clb-card-dragging');
 		  },
 		  stop: function(event, ui){
-			  $(ui.item).removeClass('dragging');
-		  },
-		  update: function(event, ui){
-			  ui.item.newIndex = ui.item.index();
-			  $scope.onUpdateCallback({item: ui.item});
+			  $(ui.item).removeClass('clb-card-dragging');
+			  
+			  var newListIndex = $(ui.item).parents('[data-list-index]').data('list-index');
+			  var newCardIndex 	= ui.item.index();
+			  // callback should only be executed if card has been actually updated among connected lists
+			  // jquery ui sortable callback has bug, it fires twice the update function 
+			  // that is why using stop function
+			  if (newCardIndex != ui.item.prevIndex || newListIndex != ui.item.prevListIndex) {
+				  ui.item.newListIndex = newListIndex;
+				  ui.item.newIndex = newCardIndex;
+				  $scope.$apply(function(){
+					$scope.onUpdateCallback({item: ui.item});  
+				  });
+				  
+			  }
 		  }
 		});
 		$( element ).disableSelection();
+		
+		
 	},
 	
-	template: '<div class="list-title">{{list.title}}</div>'+
-	'<div class="card-wraper sortable droptrue connectedSortable">'+
-		'<div clb-card ng-repeat="card in list.cards | orderBy:priorityIndex" class="card ui-state-default"></div>'+
+	template: '<div class="clb-list-title">{{list.title}}</div>'+
+	'<div class="clb-card-wraper clb-sortable clb-connected-sortable">'+
+		'<div clb-card ng-click="onCardClickCallback({card:card})" title="{{card.title}}" ng-repeat="card in list.cards | orderBy:priorityIndex" class="clb-card ui-state-default"></div>'+
 	'</div>'
   };
 }).directive('clbCard', function() {
   return {
 	restrict: 'A',
-	template: '<div class="card-label-wrap">'+
-		'<div class="card-label" style="background:{{clbCardTypes[card.type].colorCode}}"></div>'+
+	template: '<div class="clb-card-label-wrap">'+
+		'<div class="clb-card-label" style="background:{{clbCardTypes[card.type].colorCode}}"></div>'+
 	'</div>'+
-'<div class="card-title">{{card.priority}} - {{card.title}}</div>'
+'<div class="clb-card-title">{{card.priority}} - {{card.title}}</div>'
   };
 });
